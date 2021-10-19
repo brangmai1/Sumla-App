851e2bdb69bf20d89f308e60331d0cf79fc52c97
Brang Mai <brangmai100@gmail.com>
2021-10-19 15:18:07 -0400

    First commit

---
+++ Sumla/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sumla
+//
+//  Created by Brang Mai on 10/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
