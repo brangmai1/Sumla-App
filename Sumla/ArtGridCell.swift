8e9c8c40170e23fb5347453c3f5b8758d186b2f7
Brang Mai <brangmai100@gmail.com>
2021-11-14 13:10:17 -0500

    Added features to Storyboard

---
+++ Sumla/ArtGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArtGridCell.swift
+//  Sumla
+//
+//  Created by Brang Mai on 11/14/21.
+//
+
+import UIKit
+
+class ArtGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var artworkView: UIImageView!
+}
