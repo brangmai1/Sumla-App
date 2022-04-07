8d2a2a7d2dd82d1906dd93ec00f54932c36e943a
Brang Mai <brangmai100@gmail.com>
2022-04-07 16:20:27 -0400

    Add Core Data

---
+++ Sumla/sumlaListItem/Entity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Entity+CoreDataClass.swift
+//  Sumla
+//
+//  Created by Brang Mai on 4/7/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Entity)
+public class Entity: NSManagedObject {
+
+}
